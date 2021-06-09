b8b6c9d5f0b856da4190e9cd98375103b85064f3
anna.luchinina <anna.luchinina@ispring.com>
2021-06-09 10:59:53 +0300

    Добавила миграции для создания таблицы fabric

---
+++ migrations/000001_create_fabric_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `fabric`
+(
+    id BINARY(16) NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    cost FLOAT NOT NULL,
+    amount FLOAT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
